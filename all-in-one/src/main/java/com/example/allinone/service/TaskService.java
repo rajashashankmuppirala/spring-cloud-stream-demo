0d91a452f97bc10d44283698685fc86c7aa7e233
shashank <mshashank@gmail.com>
2024-12-18 23:33:29 -0500

    added support and example for rabbit mq delayed message exchange support

--- all-in-one/src/main/java/com/example/allinone/service/TaskService.java
+++ all-in-one/src/main/java/com/example/allinone/service/TaskService.java
@@ -2,12 +2,17 @@ package com.example.allinone.service;
 
 
 import dto.MyTask;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.stream.function.StreamBridge;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.support.MessageBuilder;
 import org.springframework.stereotype.Service;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @Service
+@Slf4j
 public class TaskService {
 
     private final StreamBridge streamBridge;
@@ -19,9 +24,13 @@ public class TaskService {
     }
 
     public void createTask(MyTask task){
+        Map<String, Object> headers = new HashMap<>();
+        headers.put("x-delay", 10000);
         Message<MyTask> message = MessageBuilder.withPayload(task)
                 .setHeader("contentType", "application/json")
+                .copyHeaders(headers)
                 .build();
+        log.info("before sending message");
         streamBridge.send(OUTPUT_BINDING, message);
     }
 }
---
+++ docker/README.md
@@ -0,0 +1,3 @@
+rabbitmq/delayed-exchange
+
+docker run -d --name rabbitmq-delayed -p 5672:5672 -p 15672:15672 rabbitmq/delayed-exchange:latest
